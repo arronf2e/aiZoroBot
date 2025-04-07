7ed0cd3e124381ccaace9dbd1cc07dc4770171b7
0xzz <arronf2e@163.com>
2025-04-07 16:53:25 +0800

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,9 +1,22 @@
-import chalk from 'chalk';
+let chalk;
+import('chalk').then(module => {
+    chalk = module.default;
+}).catch(err => {
+    console.error('Failed to import chalk:', err);
+    process.exit(1);
+});
 
-export const thread_delay = 30; // 随机延迟 0 到 30 秒
-export const thread_count = 5; // 线程数量
-export const sleep = ms => new Promise(resolve => setTimeout(resolve, ms)); // 休眠函数
-export const log = msg => {
+const thread_delay = 30; // 随机延迟 0 到 30 秒
+const thread_count = 5; // 线程数量
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms)); // 休眠函数
+const log = msg => {
     const time = new Date().toLocaleTimeString();
     console.log(`${chalk.gray(`[${time}]`)} ${msg}`);
 }
+
+module.exports = {
+    thread_delay,
+    thread_count,
+    sleep,
+    log
+};
